3314a5a15e8004f48b2b9ba6cb2fd31bd37bdd60
pedrotp <pedro@pedrotp.com>
2015-09-07 23:45:02 -0400

    happy new year

--- countdown.rb
+++ countdown.rb
@@ -1,4 +1,18 @@
 #write your code here
 
-def countdown
+def countdown(countdown_from)
+  while countdown_from != 0 do
+    puts "#{countdown_from} SECOND(S)!"
+    countdown_from -= 1
+  end
+  "HAPPY NEW YEAR!"
+end
+
+def countdown_with_sleep(countdown_from)
+  while countdown_from != 0 do
+    puts "#{countdown_from} SECOND(S)!"
+    countdown_from -= 1
+    sleep(1)
+  end
+  "HAPPY NEW YEAR!"
 end
